f8b4d8f5c940d9a70343368341085a570fb9581a
Paul Hofer <phofer@student.42prague.com>
2026-03-25 15:48:14 +0100

    more skeleton files

---
+++ include/cub3d.h
@@ -0,0 +1,67 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+typedef struct s_player
+{
+	double	x;
+	double	y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+}	t_player;
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
+typedef struct s_map
+{
+	char	**grid;
+	int		width;
+	int		height;
+}	t_map;
+
+typedef struct s_ray
+{
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+
+	int		map_x;
+	int		map_y;
+
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	side_dist_x;
+	double	side_dist_y;
+
+	int		step_x;
+	int		step_y;
+
+	int		hit;
+	int		side;
+
+	double	perp_wall_dist;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+}	t_ray;
+
+typedef struct s_game
+{
+	void		*mlx;
+	void		*win;
+	t_img		img;
+	t_map		map;
+	t_player	player;
+	int			floor_color;
+	int			ceiling_color;
+}	t_game;
+
+#endif
