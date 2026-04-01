2d3e618d813633989ec9e71e58409b0cb1d9826e
ctthanhthao <ctthanhthao@users.noreply.github.com>
2026-04-01 12:39:53 +0200

    Revert "arranged logic"
    This reverts commit e9fb4fdbccb89820d053bf66aac12ad27082a47f.

--- src/game/game.c
+++ src/game/game.c
@@ -12,31 +12,14 @@
 
 #include "../../includes/cub3d.h"
 
-void	init_game(t_game *game, char *map_path)
-{
-	if (parse_file(game, map_path) == FAILURE)
-	{
-		log_err("Error happened.");
-		cleanup(game);
-		exit(EXIT_FAILURE);
-	}
-	printf("floor color: %i\n", game->floor_color);
-	printf("ceiling color: %i\n", game->ceiling_color);
-	printf("NO: %s\n", game->texture_path[NORTH]);
-	printf("SO: %s\n", game->texture_path[SOUTH]);
-	printf("EA: %s\n", game->texture_path[EAST]);
-	printf("WE: %s\n", game->texture_path[WEST]);
-	for (int i = 0; i < game->map.height; i++)
-        printf("%s\n", game->map.grid[i]);
-	load_textures(game);
-}
-
+// Zeroes the game struct, runs init stages in order, then enters the loop.
 void	start_game(t_game *game, char *map)
 {
-	initialize(game);
+//	ft_memset(game, 0, sizeof(t_game)); --> game was initualized before
+// jumping here. See main funtion 
 	init_mlx(game);
-	init_game(game, map);
-	//init_game_test(game, map); //for testing only
+	//init_game(game, map); TODO: Parsing .cub, initializing
+	init_game_test(game, map); //for testing only
 	mlx_loop(game->mlx);
-	cleanup(game);
+	//game_cleanup(game); TODO:
 }
