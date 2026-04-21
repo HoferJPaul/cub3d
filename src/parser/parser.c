6987d3dc676ca8da4fc40ee855e40ccbaa1ca62e
ctthanhthao <ctthanhthao@gmail.com>
2026-04-21 13:21:18 +0200

    validate empty file

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -13,6 +13,25 @@
 #include "cub3d.h"
 #include "parser.h"
 
+static bool	is_file_empty(char **lines)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (lines[i])
+	{
+		j = 0;
+		while (lines[i][j])
+		{
+			if (!ft_isspace(lines[i][j]) && lines[i][j] != '\n')
+				return (false);
+			j++;
+		}
+		i++;
+	}
+	return (true);
+}
 /**
  * @brief This function is the orchestrator of the entire configuration file 
  * parsing workflow. It reads a configuration file into memory, sequentially 
@@ -28,6 +47,11 @@ int	parse_file(t_game *game, const char *filename)
 	lines = read_file_lines(filename, &line_count);
 	if (!lines)
 		return (FAILURE);
+	if (is_file_empty(lines))
+	{
+		log_err("File is empty.")
+		return (FAILURE);
+	}
 	if (parse_textures(game, lines, line_count) != SUCCESS)
 		return (free_arr(lines), FAILURE);
 	if (parse_colors(game, lines, line_count) != SUCCESS)
